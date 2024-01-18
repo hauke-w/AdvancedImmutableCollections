dd26684d081402fb82b29084346d626e47d88bdf
Hauke Wittern <hauke.wittern@web.de>
2024-01-18 21:36:15 +0100

    implement IImmutableSet

--- AdvancedImmutableCollections/ImmutableHashSetValue`1.cs
+++ AdvancedImmutableCollections/ImmutableHashSetValue`1.cs
@@ -4,7 +4,7 @@
 /// Immutable hash set with value semantics.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public readonly struct ImmutableHashSetValue<T> : IReadOnlyCollection<T>, IEquatable<ImmutableHashSetValue<T>>
+public readonly struct ImmutableHashSetValue<T> : IImmutableSet<T>, IEquatable<ImmutableHashSetValue<T>>
 {
     private readonly ImmutableHashSet<T> _Value;
 
@@ -19,14 +19,6 @@ public readonly struct ImmutableHashSetValue<T> : IReadOnlyCollection<T>, IEquat
     }
 
     public ImmutableHashSet<T> Value => _Value ?? ImmutableHashSet<T>.Empty;
-
-    public ImmutableHashSetValue<T> Add(T item) => _Value.Add(item).WithValueSemantics();
-
-    public ImmutableHashSetValue<T> AddRange(IEnumerable<T> items) => _Value.Union(items).WithValueSemantics();
-
-    public ImmutableHashSetValue<T> Remove(T item) => _Value is not { Count: > 0 } ? this : _Value.Remove(item).WithValueSemantics();
-
-    public ImmutableHashSetValue<T> Except(IEnumerable<T> items) => _Value is not { Count: > 0 } ? this : _Value.Except(items).WithValueSemantics();
 
     public bool Contains(T item) => _Value is not null && _Value.Contains(item);
 
@@ -65,4 +57,46 @@ public readonly struct ImmutableHashSetValue<T> : IReadOnlyCollection<T>, IEquat
 
     public static implicit operator ImmutableHashSet<T>(ImmutableHashSetValue<T> value) => value._Value;
     public static implicit operator ImmutableHashSetValue<T>(ImmutableHashSet<T> value) => new ImmutableHashSetValue<T>(value);
+
+    #region mutation operations
+    public ImmutableHashSetValue<T> Add(T item) => Value.Add(item).WithValueSemantics();
+
+    public ImmutableHashSetValue<T> AddRange(IEnumerable<T> items) => Value.Union(items).WithValueSemantics();
+
+    public ImmutableHashSetValue<T> Remove(T item) => _Value is not { Count: > 0 } ? this : _Value.Remove(item).WithValueSemantics();
+
+    public ImmutableHashSetValue<T> Except(IEnumerable<T> items) => _Value is not { Count: > 0 } ? this : _Value.Except(items).WithValueSemantics();
+
+    public ImmutableHashSetValue<T> Clear() => _Value is not { Count: > 0 } ? this : new ImmutableHashSetValue<T>(ImmutableHashSet<T>.Empty);
+    public ImmutableHashSetValue<T> SymmetricExcept(IEnumerable<T> other) => _Value is not { Count: > 0 } ? this : _Value.SymmetricExcept(other).WithValueSemantics();
+    public ImmutableHashSetValue<T> Intersect(IEnumerable<T> other) => _Value is not { Count: > 0 } ? this : _Value.Intersect(other).WithValueSemantics();
+    public ImmutableHashSetValue<T> Union(IEnumerable<T> other) => Value.Union(other).WithValueSemantics();
+    #endregion
+
+    #region IImmutableSet
+    IImmutableSet<T> IImmutableSet<T>.Add(T value) => Add(value);
+    IImmutableSet<T> IImmutableSet<T>.Clear() => Clear();
+    IImmutableSet<T> IImmutableSet<T>.Except(IEnumerable<T> other) => Except(other);
+    IImmutableSet<T> IImmutableSet<T>.Intersect(IEnumerable<T> other) => Intersect(other);
+    public bool IsProperSubsetOf(IEnumerable<T> other) => _Value.IsProperSubsetOf(other);
+    public bool IsProperSupersetOf(IEnumerable<T> other) => _Value.IsProperSupersetOf(other);
+    public bool IsSubsetOf(IEnumerable<T> other) => _Value.IsSubsetOf(other);
+    public bool IsSupersetOf(IEnumerable<T> other) => _Value.IsSupersetOf(other);
+    public bool Overlaps(IEnumerable<T> other) => _Value.Overlaps(other);
+    IImmutableSet<T> IImmutableSet<T>.Remove(T value) => Remove(value);
+    public bool SetEquals(IEnumerable<T> other) => _Value.SetEquals(other);
+    IImmutableSet<T> IImmutableSet<T>.SymmetricExcept(IEnumerable<T> other) => SymmetricExcept(other);
+
+    public bool TryGetValue(T equalValue, out T actualValue)
+    {
+        if (_Value is not { Count:>0 })
+        {
+            actualValue = equalValue;
+            return false;
+        }
+        return _Value.TryGetValue(equalValue, out actualValue);
+    }
+
+    IImmutableSet<T> IImmutableSet<T>.Union(IEnumerable<T> other) => Union(other);
+    #endregion
 }

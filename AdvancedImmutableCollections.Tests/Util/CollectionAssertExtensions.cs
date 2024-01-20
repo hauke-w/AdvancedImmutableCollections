33c4ce7387a749607e541400881974bbdbde9136
Hauke Wittern <hauke.wittern@web.de>
2024-01-21 00:53:14 +0100

    implemented AreEquivalent

--- AdvancedImmutableCollections.Tests/Util/CollectionAssertExtensions.cs
+++ AdvancedImmutableCollections.Tests/Util/CollectionAssertExtensions.cs
@@ -21,7 +21,55 @@ internal static class CollectionAssertExtensions
     public static void AreEquivalent<T>(this CollectionAssert that, IEnumerable<T> expected, IEnumerable<T> actual, IEqualityComparer<T>? itemComparer)
     {
         // TODO: CollectionAssert does not support AreEquivalent with comparer
-        throw new NotImplementedException();
+        var expectedItems = new Dictionary<T, int>(itemComparer);
+        int expectedNullCount = 0;
+        int expectedCount = 0;
+        foreach (var item in expected)
+        {
+            expectedCount++;
+            if (item is null)
+            {
+                expectedNullCount++;
+            }
+            else if (expectedItems.TryGetValue(item, out int count))
+            {
+                expectedItems[item] = count + 1;
+            }
+            else
+            {
+                expectedItems.Add(item, 1);
+            }
+        }
+
+        var remainingItems = new Dictionary<T, int>(expectedItems, itemComparer);
+        int remainingNullCount = 0;
+        int actualCount = 0;
+        foreach (var item in actual)
+        {
+            actualCount++;
+            if (item is null)
+            {
+                if (remainingNullCount <= 0)
+                {
+                    Assert.Fail($"null value occurs more than expect: {expectedNullCount}");
+                }
+                remainingNullCount--;
+            }
+            else if (remainingItems.TryGetValue(item, out int count))
+            {
+                if (count <= 0)
+                {
+                    Assert.Fail($"item {item} occurs more often than expected: {expectedItems[item]}");
+                }
+                remainingItems[item] = count - 1;
+            }
+            else
+            {
+                Assert.Fail($"item {item} does not occur in expected items");
+            }
+        }
+        Assert.AreEqual(0, remainingNullCount, "null value does not occur as often as expected");
+        Assert.AreEqual(expectedCount, actualCount);
     }
 
     private class ItemComparer<T> : IComparer

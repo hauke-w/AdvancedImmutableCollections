3f9c861f97de576d9c9c1d365711c48ebff161a3
Hauke Wittern <9463839+hauke-w@users.noreply.github.com>
2024-03-28 17:36:58 +0100

    added tests

---
+++ AdvancedImmutableCollections.Tests/CollectionAdapters/ImmutableHashSetValueAdapterTests.cs
@@ -0,0 +1,38 @@
+﻿namespace AdvancedImmutableCollections.Tests.CollectionAdapters;
+
+[TestClass]
+public class ImmutableHashSetValueAdapterTests
+{
+    [TestMethod]
+    public void ConstructorTest()
+    {
+        var collection = ImmutableHashSetValue.Create<GenericParameterHelper>();
+        var actual = new ImmutableHashSetValueAdapter<GenericParameterHelper>(collection);
+        Assert.AreEqual(collection, actual.Collection);
+        Assert.AreEqual(EqualityComparer<GenericParameterHelper>.Default, actual.Collection.Value.KeyComparer);
+    }
+
+    [TestMethod]
+    public void Constructor_Array_Test()
+    {
+        GenericParameterHelper[] items = [new GenericParameterHelper(1), new GenericParameterHelper(2)];
+        var actual = new ImmutableHashSetValueAdapter<GenericParameterHelper>(items);
+        CollectionAssert.AreEquivalent(items, actual.ToList());
+        Assert.AreEqual(EqualityComparer<GenericParameterHelper>.Default, actual.Collection.Value.KeyComparer);
+    }
+
+    [TestMethod]
+    public void Constructor_IEqualityComparer_Array_Test()
+    {
+        Constructor_IEqualityComparer_Array_Test(["a", "b", "C"], StringComparer.Ordinal);
+        Constructor_IEqualityComparer_Array_Test(["a", "b", "C"], null);
+
+        static void Constructor_IEqualityComparer_Array_Test(string[] items, IEqualityComparer<string>? comparer)
+        {
+            var actual = new ImmutableHashSetValueAdapter<string>(comparer, items);
+            CollectionAssert.AreEquivalent(items, actual.ToList());
+            var expectedComparer = comparer ?? EqualityComparer<string>.Default;
+            Assert.AreEqual(expectedComparer, actual.Collection.Value.KeyComparer);
+        }
+    }
+}
---
+++ AdvancedImmutableCollections.Tests/CollectionAdapters/ImmutableSortedSetValueAdapterFactoryTests.cs
@@ -0,0 +1,142 @@
+﻿namespace AdvancedImmutableCollections.Tests.CollectionAdapters;
+
+[TestClass]
+public class ImmutableSortedSetValueAdapterFactoryTests
+{
+    [TestMethod]
+    public void IImmutableSetAdapterFactory_GetDefaultTest()
+    {
+        IImmutableSetAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        var actual = testObject.GetDefaultValue<GenericParameterHelper>();
+        VerifyIsDefault(actual);
+    }
+
+    [TestMethod]
+    public void IImmutableCollectionAdapterFactory_GetDefaultTest()
+    {
+        IImmutableCollectionAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        var actual = testObject.GetDefaultValue<GenericParameterHelper>();
+        VerifyIsDefault(actual);
+    }
+
+    private static void VerifyIsDefault(IImmutableCollectionAdapter<GenericParameterHelper>? actual)
+    {
+        Assert.IsNotNull(actual);
+        Assert.IsInstanceOfType(actual, typeof(ImmutableSortedSetValueAdapter<GenericParameterHelper>));
+        Assert.AreEqual(default(ImmutableSortedSetValue<GenericParameterHelper>), actual.Collection);
+        Assert.IsTrue(((ImmutableSortedSetValue<GenericParameterHelper>)actual.Collection).IsDefault);
+    }
+
+    [TestMethod]
+    public void IImmutableSetAdapterFactory_CastTest()
+    {
+        IImmutableSetAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        var collection = ImmutableSortedSetValue.Create(new GenericParameterHelper(1), new GenericParameterHelper(2));
+        var actual = testObject.Cast(collection);
+        VerifyCast(actual, collection);
+    }
+
+    [TestMethod]
+    public void IImmutableCollectionAdapterFactory_CastTest()
+    {
+        IImmutableCollectionAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        var collection = ImmutableSortedSetValue.Create(new GenericParameterHelper(1), new GenericParameterHelper(2));
+        var actual = testObject.Cast(collection);
+        VerifyCast(actual, collection);
+    }
+
+    private void VerifyCast(IImmutableCollectionAdapter<GenericParameterHelper> actual, ImmutableSortedSetValue<GenericParameterHelper> expected)
+    {
+        Assert.IsNotNull(actual);
+        Assert.IsInstanceOfType(actual, typeof(ImmutableSortedSetValueAdapter<GenericParameterHelper>));
+        Assert.IsInstanceOfType(actual.Collection, typeof(ImmutableSortedSetValue<GenericParameterHelper>));
+        CollectionAssert.AreEquivalent(expected.ToList(), actual.Collection.ToList());
+    }
+
+    [TestMethod]
+    public void IImmutableSetAdapterFactory_CreateTest()
+    {
+        IImmutableSetAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        GenericParameterHelper[] items = [new GenericParameterHelper(1), new GenericParameterHelper(3), new GenericParameterHelper(2)];
+        var actual = testObject.Create(items);
+        VerifyCreate(items, actual);
+    }
+
+    [TestMethod]
+    public void IImmutableCollectionAdapterFactory_CreateTest()
+    {
+        IImmutableCollectionAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        GenericParameterHelper[] items = [new GenericParameterHelper(1), new GenericParameterHelper(3), new GenericParameterHelper(2)];
+        var actual = testObject.Create(items);
+        VerifyCreate(items, actual);
+    }
+
+    /// <summary>
+    /// Verifies <see cref="IImmutableSetAdapterFactory.Create{T}(IEnumerable{T})"/> implementation.
+    /// </summary>
+    [TestMethod]
+    public void IImmutableSetAdapterFactory_Create_IEnumerable_Test()
+    {
+        IImmutableSetAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        GenericParameterHelper[] items = [new GenericParameterHelper(1), new GenericParameterHelper(3), new GenericParameterHelper(2)];
+        var actual = testObject.Create((IEnumerable<GenericParameterHelper>)items);
+        VerifyCreate(items, actual);
+    }
+
+    /// <summary>
+    /// Verifies <see cref="IImmutableSortedSetAdapterFactory.Create{T}(IComparer{T}?, T[])"/> implementation.
+    /// </summary>
+    [TestMethod]
+    public void IImmutableSortedSetAdapterFactory_Create_IComparer_Array_Test()
+    {
+        IImmutableSortedSetAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        string[] items = ["a", "C", "b", "A"];
+        var actual = testObject.Create(StringComparer.InvariantCulture, items);
+        VerifyCreate(["a", "A", "b", "C"], actual, StringComparer.InvariantCulture);
+
+        actual = testObject.Create(StringComparer.InvariantCultureIgnoreCase, items);
+        VerifyCreate(["a", "b", "C"], actual, StringComparer.InvariantCultureIgnoreCase);
+    }
+
+    /// <summary>
+    /// Verifies <see cref="IImmutableSortedSetAdapterFactory.Create{T}(IComparer{T}?)"/> implementation.
+    /// </summary>
+    [TestMethod]
+    public void IImmutableSetWithEqualityComparerAdapterFactory_Create_IComparer_Test()
+    {
+        IImmutableSortedSetAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        var actual = testObject.Create(StringComparer.Ordinal);
+        VerifyCreate([], actual, StringComparer.Ordinal);
+
+        actual = testObject.Create(StringComparer.OrdinalIgnoreCase);
+        VerifyCreate([], actual, StringComparer.OrdinalIgnoreCase);
+    }
+
+    private static void VerifyCreate<T>(T[] expected, IImmutableCollectionAdapter<T> actual, IComparer<T>? expectedComparer = null)
+    {
+        Assert.IsNotNull(actual);
+        Assert.IsInstanceOfType(actual, typeof(ImmutableSortedSetValueAdapter<T>));
+        Assert.IsInstanceOfType(actual.Collection, typeof(ImmutableSortedSetValue<T>));
+        CollectionAssert.AreEquivalent(expected, actual.Collection.ToList());
+
+        var actualCollection = (ImmutableSortedSetValue<T>)actual.Collection;
+        expectedComparer ??= Comparer<T>.Default;
+        Assert.AreEqual(expectedComparer, actualCollection.Value.KeyComparer);
+    }
+
+    [TestMethod]
+    public void IImmutableCollectionAdapterFactory_CreateMutableTest()
+    {
+        IImmutableCollectionAdapterFactory testObject = new ImmutableSortedSetValueAdapterFactory();
+        GenericParameterHelper[] items = [new GenericParameterHelper(1), new GenericParameterHelper(3), new GenericParameterHelper(2)];
+        var actual = testObject.CreateMutable(items);
+        VerifyCreateMutable(items, actual);
+    }
+
+    private static void VerifyCreateMutable(GenericParameterHelper[] items, ICollection<GenericParameterHelper> actual)
+    {
+        Assert.IsNotNull(actual);
+        Assert.IsInstanceOfType(actual, typeof(SortedSet<GenericParameterHelper>));
+        CollectionAssert.AreEquivalent(items, actual.ToList());
+    }
+}
